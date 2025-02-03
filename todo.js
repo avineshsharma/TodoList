9e9847aecc1855da89cbe9b0c6c58cc698981252
avineshsharma <jangraavinesh@gmail.com>
2025-02-03 21:42:46 +0530

    final

--- todo.js
+++ todo.js
@@ -4,9 +4,15 @@ submit.addEventListener("click", (e) => {
     let cann = can.value
     localStorage.setItem("todo", JSON.stringify([titlee, cann]))
     console.log(e)
+    toodo.innerHTML = `
+     <h1>${titlee}</h1>
+     <p>${cann}</p>`
+    title.value = ""
+    can.value = ""
 })
 
 delet.addEventListener("click", (e) => {
     e.preventDefault()
     localStorage.removeItem("todo")
+    toodo.innerHTML = ""
 })
